41e856707348859bcc92a1e33755ac02f273d977
Raul Torres <raulunihamburg@googlemail.com>
2023-11-05 18:10:06 -0500

    [NFC] Structure nested switches as functions

--- lib/codegen/c++/Generator.cpp
+++ lib/codegen/c++/Generator.cpp
@@ -15,45 +15,53 @@ static std::string GetWidthAsString(RangeWidth Width) {
   }
 }
 
+static void GenerateCPUCode(Kernel &K, ComputingUnit TargetCU) {
+  switch (TargetCU) {
+  case ComputingUnit::THREAD:
+    for (auto [Ind, Range] : K.GetRanges()) {
+      std::cout << fmt::format(
+          "  #pragma omp parallel for\n"
+          "  for ( {0} {1} = {2}; {1} < {3}; {1} += {4} ) {{\n"
+          "  \n"
+          "  }}\n",
+          GetWidthAsString(Range.GetWidth()), Ind, Range.GetLowerbound(),
+          Range.GetUpperbound(), Range.GetStride());
+    }
+    break;
+  default:
+    break;
+  }
+}
+
+static void GenerateAccelCode(Kernel &K, ComputingUnit TargetCU) {
+  switch (TargetCU) {
+  case ptk::ComputingUnit::TEAM:
+    for (auto [Ind, Range] : K.GetRanges()) {
+      std::cout << fmt::format(
+          "  #pragma omp target teams distribute\n"
+          "  for ( {0} {1} = {2}; {1} < {3}; {1} += {4} ) {{\n"
+          "  \n"
+          "  }}\n",
+          GetWidthAsString(Range.GetWidth()), Ind, Range.GetLowerbound(),
+          Range.GetUpperbound(), Range.GetStride());
+    }
+    break;
+  default:
+    break;
+  }
+}
+
 void GenerateKernel(Kernel &K) {
-  auto [Source, Target] = K.GetDistribution();
+  auto [SourceCU, TargetCU] = K.GetDistribution();
 
   std::cout << fmt::format("void {0} () {{\n", K.GetName());
 
-  switch (Source) {
+  switch (SourceCU) {
   case ptk::ComputingUnit::CPU:
-    switch (Target) {
-    case ptk::ComputingUnit::THREAD:
-      for (auto [Ind, Range] : K.GetRanges()) {
-        std::cout << fmt::format(
-            "  #pragma omp parallel for\n"
-            "  for ( {0} {1} = {2}; {1} < {3}; {1} += {4} ) {{\n"
-            "  \n"
-            "  }}\n",
-            GetWidthAsString(Range.GetWidth()), Ind, Range.GetLowerbound(),
-            Range.GetUpperbound(), Range.GetStride());
-      }
-      break;
-    default:
-      break;
-    }
+    GenerateCPUCode(K, TargetCU);
     break;
   case ptk::ComputingUnit::ACCEL:
-    switch (Target) {
-    case ptk::ComputingUnit::TEAM:
-      for (auto [Ind, Range] : K.GetRanges()) {
-        std::cout << fmt::format(
-            "  #pragma omp target teams distribute\n"
-            "  for ( {0} {1} = {2}; {1} < {3}; {1} += {4} ) {{\n"
-            "  \n"
-            "  }}\n",
-            GetWidthAsString(Range.GetWidth()), Ind, Range.GetLowerbound(),
-            Range.GetUpperbound(), Range.GetStride());
-      }
-      break;
-    default:
-      break;
-    }
+    GenerateAccelCode(K, TargetCU);
     break;
   default:
     break;
